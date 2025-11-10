08c626d45cb399edd81ff9eccfe44c303fd6d9bc
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-10 09:12:58 +0000

    06 /  Ternäre (ternary) Schreibweise

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -38,9 +38,16 @@ console.log("ageMark: " + ageMark);
 // if (true)
 // if (false) 
 // if(isJohnOlder)
-if(ageJohn > ageMark)
-{     
-    console.log("John ist älter.");
-} else {
-     console.log("John ist jünger.");
-}
+// if(ageJohn > ageMark)
+// {     
+//     console.log("John ist älter.");
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
